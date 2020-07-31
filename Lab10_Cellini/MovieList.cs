0d11b1ed534a34b8e0f12f1349e293069c8d580c
CelliniJ <68167256+CelliniJ@users.noreply.github.com>
2020-07-30 23:58:12 -0400

    finished

---
+++ Lab10_Cellini/MovieList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10_Cellini
+{
+    class Movie
+    {
+        #region fields
+        private string title;
+        private string category;
+        #endregion
+
+        #region properties
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        #endregion
+
+        #region constructors
+
+        //default constructor
+        public Movie() { }
+
+        // overloaded constructor
+        public Movie(string Title, string Category)
+        {
+            title = Title;
+            category = Category;
+        }
+        #endregion
+
+    
+    }
+}
---
+++ Lab10_Cellini/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10_Cellini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> MovieList = new List<Movie>() { };
+
+            MovieList.Add(new Movie("Star Wars", "Sci-Fi"));
+            MovieList.Add(new Movie("Minority Report", "Sci-Fi"));
+            MovieList.Add(new Movie("Blade Runner", "Sci-Fi"));
+            MovieList.Add(new Movie("Independence Day", "Sci-Fi"));
+            MovieList.Add(new Movie("The Martian", "Sci-Fi"));
+
+            MovieList.Add(new Movie("Get Out", "Horror"));
+            MovieList.Add(new Movie("Us", "Horror"));
+            MovieList.Add(new Movie("Midsommar", "Horror"));
+            MovieList.Add(new Movie("Hereditary", "Horror"));
+            MovieList.Add(new Movie("The Exorcist", "Horror"));
+
+            MovieList.Add(new Movie("Parasite", "Drama"));
+            MovieList.Add(new Movie("The Irishman", "Drama"));
+            MovieList.Add(new Movie("Dunkirk", "Drama"));
+            MovieList.Add(new Movie("The Departed", "Drama"));
+            MovieList.Add(new Movie("A Few Good Men", "Drama"));
+
+            MovieList.Add(new Movie("Toy Story", "Animated"));
+            MovieList.Add(new Movie("The Lion King", "Animated"));
+            MovieList.Add(new Movie("Hercules", "Animated"));
+            MovieList.Add(new Movie("Aladdin", "Animated"));
+            MovieList.Add(new Movie("The Emporer's New Groove", "Animated"));
+
+
+            Console.WriteLine("Welcome to the Movie List Application");
+            Console.WriteLine($"There are {MovieList.Count} in this list.");
+
+            string repeat = "y";
+            while (repeat == "y")
+            {
+                Console.WriteLine("What category are you interested in? (Sci-fi, Horror, Drama, Animated): ");
+                string categoryinput = Console.ReadLine().ToLower();
+
+                while (categoryinput != "sci-Fi" && categoryinput != "horror" && categoryinput != "drama" && categoryinput != "animated")
+                {
+                    Console.WriteLine("Invalid input. Please try again");
+                    categoryinput = Console.ReadLine();
+                    continue;
+                }
+
+                for (int i = 0; i < MovieList.Count; i++)
+                {
+
+                    if (categoryinput == "sci-fi")
+                    {
+                        if (MovieList[i].Category == "Sci-Fi")
+                        {
+                            Console.WriteLine(MovieList[i].Title);
+                        }
+                    }
+                    else if (categoryinput == "horror")
+                    {
+                        if (MovieList[i].Category == "Horror")
+                        {
+                            Console.WriteLine(MovieList[i].Title);
+                        }
+                    }
+                    else if (categoryinput == "drama")
+                    {
+                        if (MovieList[i].Category == "Drama")
+                        {
+                            Console.WriteLine(MovieList[i].Title);
+                        }
+                    }
+                    else if (categoryinput == "animated")
+                    {
+                        if (MovieList[i].Category == "Animated")
+                        {
+                            Console.WriteLine(MovieList[i].Title);
+                        }
+                    }
+                }
+                    Console.WriteLine("Continue? (y/n): ");
+                    repeat = Console.ReadLine().ToLower();
+
+                while(repeat != "y" && repeat != "n")
+                {
+                    Console.WriteLine("Invalid input. Continue? (y/n): ");
+                    repeat = Console.ReadLine();
+                }
+                Console.WriteLine("See ya!");
+                }
+            }
+        }
+    }
